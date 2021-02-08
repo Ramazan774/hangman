c58a765876956630df45c595fae32097795b4f8b
Ramazan774 <ramazan.ramazanov@glion.ch>
2021-02-07 21:20:03 -0800

    components added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import Header from "./components/Header";
 import Figure from "./components/Figure";
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
 
 import "./App.css";
+
+const words = ['application', 'programming', 'interface', 'wizard']
+let selectedWord = words[Math.floor(Math.random() * words.length)]
+
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
     </>
   );
 }
